8e1342d3a86201a4024147109f0d95860cd06731
md-al-mamun22 <ahmedmdmdamun18@gmail.com>
2026-06-25 12:52:48 +0600

    Day 1: Stign + Number

---
+++ string.py
@@ -0,0 +1,14 @@
+print ("Mamun's a pen")
+print ("mamun " * 5)
+
+name = "Mamun"
+print (name)
+
+
+firstName = ("Md AL ")
+lastName = ("Mamun")
+
+print (firstName +  lastName)
+
+mobile = 195467829
+print (mobile)
